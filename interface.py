1cbb2867884041d360bd7978a4191c0aefe49a72
xavier <diego.mora8157@alumnos.udg.mx>
2023-03-19 22:48:18 -0600

    done, still need cleaning and refactoring and restart funcion to work properly

--- interface.py
+++ interface.py
@@ -16,38 +16,37 @@ class Aplicacion():
      self.raiz.geometry('600x400')
      self.raiz.title('FCFS Algorithm')     
 
-
-
-     self.canvas1 = Canvas(self.raiz, width=width , height=60, bg="green")
-     self.canvas1.place(x=0,y=0)
-     self.canvas2 = Canvas(self.raiz, width=width , height=60, bg="cornsilk")
-     self.canvas2.place(x=0,y=1*canvasVertticalSeparation)
-     self.canvas3 = Canvas(self.raiz, width=width , height=60, bg="bisque")
-     self.canvas3.place(x=0,y=2*canvasVertticalSeparation)
-     self.canvas4 = Canvas(self.raiz, width=width , height=60, bg="CadetBlue1")
-     self.canvas4.place(x=0,y=3*canvasVertticalSeparation)
-     self.canvas5 = Canvas(self.raiz, width=width , height=60, bg="Coral")
-     self.canvas5.place(x=0,y=4*canvasVertticalSeparation)
+     self.canvas = []
+     self.canvas.append( Canvas(self.raiz, width=width , height=60, bg="green"))
+     self.canvas[0].place(x=0,y=0)
+     self.canvas.append(Canvas(self.raiz, width=width , height=60, bg="cornsilk"))
+     self.canvas[1].place(x=0,y=1*canvasVertticalSeparation)
+     self.canvas.append(Canvas(self.raiz, width=width , height=60, bg="bisque"))
+     self.canvas[2].place(x=0,y=2*canvasVertticalSeparation)
+     self.canvas.append(Canvas(self.raiz, width=width , height=60, bg="CadetBlue1"))
+     self.canvas[3].place(x=0,y=3*canvasVertticalSeparation)
+     self.canvas.append(Canvas(self.raiz, width=width , height=60, bg="Coral"))
+     self.canvas[4].place(x=0,y=4*canvasVertticalSeparation)
 
      self.ufoImgOBJ = PhotoImage(file='./ufor.png')
      # self.ufoImg = self.canvas1.create_image(0,0,anchor=NW,image=self.ufoImgOBJ)
      self.ufoImg = []
 
-     self.ufoImg.append(self.canvas1.create_image(0,0,anchor=NW,image=self.ufoImgOBJ))
-     self.ufoImg.append(self.canvas2.create_image(0,0,anchor=NW,image=self.ufoImgOBJ))
-     self.ufoImg.append(self.canvas3.create_image(0,0,anchor=NW,image=self.ufoImgOBJ))
-     self.ufoImg.append(self.canvas4.create_image(0,0,anchor=NW,image=self.ufoImgOBJ))
-     self.ufoImg.append(self.canvas5.create_image(0,0,anchor=NW,image=self.ufoImgOBJ))
+     self.ufoImg.append(self.canvas[0].create_image(0,0,anchor=NW,image=self.ufoImgOBJ))
+     self.ufoImg.append(self.canvas[1].create_image(0,0,anchor=NW,image=self.ufoImgOBJ))
+     self.ufoImg.append(self.canvas[2].create_image(0,0,anchor=NW,image=self.ufoImgOBJ))
+     self.ufoImg.append(self.canvas[3].create_image(0,0,anchor=NW,image=self.ufoImgOBJ))
+     self.ufoImg.append(self.canvas[4].create_image(0,0,anchor=NW,image=self.ufoImgOBJ))
 
-     self.btnReiniciar = Button(self.raiz, text='Reiniciar')
+     self.btnReiniciar = Button(self.raiz, text='Reiniciar', command=self.reiniciar)
      self.btnReiniciar.place(x=150,y=380)
 
      ttk.Button(self.raiz, text='Salir', command=self.raiz.destroy).pack(side=BOTTOM)
 
-     processQueue = [[self.canvas1,0,300],[self.canvas2,1,200],[self.canvas3,2,90],[self.canvas4,3,400],[self.canvas5,4,100]]
+     processQueue = [[self.canvas[0],0,110],[self.canvas[1],1,200],[self.canvas[2],2,90],[self.canvas[3],3,20],[self.canvas[4],4,100]]
 
 
-     self.btnCorrer = Button(self.raiz,text='Correr',command= lambda:self.firstComeFirstServed(processQueue))
+     self.btnCorrer = Button(self.raiz,text='Correr',command= lambda:self.firstComeFirstServed(processQueue,0))
      self.btnCorrer.place(x=20, y=380)
 
 
@@ -56,21 +55,23 @@ class Aplicacion():
     def avanza(self,canvas,index,delay):
             if(self.i[index] < MAX_UFO_RECORRIDO):
                 self.i[index]+=1
-                self.canvas1.move(self.ufoImg[index],self.i[index],0)
+                self.canvas[index].move(self.ufoImg[index],self.i[index],0)
                 canvas.after(delay,lambda: self.avanza(canvas,index,delay))
                 return 0
             print(self.i[index])
 
 
-    def firstComeFirstServed(self,queue):
-        print("esta es la listap wey",type(queue))
-        print(queue)
+    def firstComeFirstServed(self,queue,delayAnterior):
         self.avanza(queue[0][0],queue[0][1],queue[0][2])
-        queue[0][0].after(MAX_UFO_RECORRIDO*queue[0][2],lambda:(queue[0][0],queue[0][1],queue[0][2]))
+        delayRecorrido  = MAX_UFO_RECORRIDO*queue[0][2] 
         if(len(queue)>1):
             queue.pop(0)
-            self.firstComeFirstServed(queue)
+            self.raiz.after(delayRecorrido,lambda:self.firstComeFirstServed(queue,delayRecorrido))
 
 
+    def reiniciar(self):
+        for i in range(5):
+            self.i[i] = 0
+            self.canvas[i].move(self.ufoImg[i],0,0)
 
 Aplicacion()
